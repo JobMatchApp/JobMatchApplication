003c3d209f966c079ba3b1a1e04c4263e2f78fad
axel <axelcaron7@outlook.fr>
2023-08-22 19:34:27 +0200

    Ajout des elements

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Etes vous sur ?',
+      cssClass: 'custom-alert',
+      buttons: [
+        {
+          text: 'Non',
+          cssClass: 'alert-button-cancel',
+        },
+        {
+          text: 'Oui',
+          cssClass: 'alert-button-confirm',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
