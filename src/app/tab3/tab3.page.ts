beac6fec586a27736d874a025e5ea76b7810d4ed
axel <axelcaron7@outlook.fr>
2023-09-07 11:22:31 +0200

    config profil

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -28,4 +28,40 @@ export class Tab3Page {
 
     await alert.present();
   }
+
+  sauvegarderClicked() {
+    // Récupérez les éléments HTML
+    const resultDiv = document.getElementById('result');
+    console.log(resultDiv);
+    if (resultDiv) {
+      const prenomInput = document.getElementById('prenomInput') as HTMLInputElement;
+      const nomInput = document.getElementById('nomInput') as HTMLInputElement;
+      const ageInput = document.getElementById('ageInput') as HTMLInputElement;
+      const villeInput = document.getElementById('villeInput') as HTMLInputElement;
+      const metierInput = document.getElementById('metierInput') as HTMLInputElement;
+      const bioInput = document.getElementById('bioInput') as HTMLInputElement;
+  
+      // Récupérez les valeurs saisies par l'utilisateur
+      const prenom = prenomInput.value;
+      const nom = nomInput.value;
+      const age = ageInput.value;
+      const ville = villeInput.value;
+      const metier = metierInput.value;
+      const bio = bioInput.value;
+  
+      // Vérifiez à nouveau si resultDiv existe (par mesure de sécurité)
+      if (resultDiv) {
+        resultDiv.innerHTML = `
+          <p>Prénom: ${prenom}</p>
+          <p>Nom: ${nom}</p>
+          <p>Age: ${age}</p>
+          <p>Ville: ${ville}</p>
+          <p>Métier: ${metier}</p>
+          <p>Bio: ${bio}</p>
+        `;
+      }
+    }
+  }
 }
+
+
