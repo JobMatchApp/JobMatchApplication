e240d790e8c149d32d9d2465c72481f88ebb656a
Axel <axel.caron@ynov.com>
2023-09-09 19:33:10 +0200

    modif chat

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  users = [
+    {
+      name: 'Cedric',
+      age: 22,
+      avatar: 'assets/photo/tof13.jpg',
+      online: true,
+      lastMessage: 'Salut, comment ça va ?',
+    },
+    {
+      name: 'Megan',
+      age: 25,
+      avatar: 'assets/photo/tof12.jpg',
+      online: false,
+      lastMessage: 'Bonjour !',
+    },
+    {
+      name: 'Eric',
+      age: 22,
+      avatar: 'assets/photo/tof14.jpg',
+      online: true,
+      lastMessage: 'Quoi de neuf ?',
+    },
+    {
+      name: 'Céline test',
+      age: 28,
+      avatar: 'assets/photo/tof15.jpg',
+      online: true,
+      lastMessage: 'Salut, ça fait longtemps !',
+    },
+  ];
   constructor() {}
 
 }
